3fb34ee6a65dc42cf9ace9152b0adb8548fe0e1d
Gregorio <Greg.del801@gmail.com>
2023-06-28 13:28:28 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// is an equation whose multiplied results will equal 62
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2
+
+// generates a random integer greater than 0
+let random = Math.floor(Math.random()) + 1
+
+// is an equation that calculates a remainder that is equal to 4
+const num3 = 16
+const num4 = 6
+
+let mod = num3 % num4
+
+// returns 20 as the highest number in the set
+let max = Math.max(1, 2, 3, 20)
